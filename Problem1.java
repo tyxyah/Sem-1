f5ce96b41f9fd078afb33850c6b22d4f5995b720
Siti Aisyah Binti Razali <78149732+tyxyah@users.noreply.github.com>
2021-03-21 17:37:42 +0800

    Add files via upload

--- Problem1.java
+++ Problem1.java
@@ -1,61 +1,55 @@
 /*************************************************************************************/
-/* This is a program that check whether a number is a palindrome or not.             */
+/* This is a program that reads the integers between 1 and 50                       */
+/* and counts the occurrences of each. The input ends with 0.                        */
 /* Course Name : Computer Programming I                                              */
 /* Course Code : SSK3100 Group 3                                                     */
-/* Assignment and Question Number : Assignment 6, Q1                                 */
+/* Assignment and Question Number : Assignment 7, Q1                                 */
 /* Name : Siti Aisyah Binti Razali                                                   */
-/* Date created : 30 December 2020                                                   */
+/* Date created : 18 January 2021                                                    */
 /*************************************************************************************/
-package assignment6;
+package assignment7;
 import java.util.*;
 public class Problem1 {
 
-	//method to return the reversal of an integer
-	public static int reverse(int num) {
-		//to declare and initialized variables
-		int reverse = 0;
-		int current = 0;
-		
-		while(num != 0) { //condition for the loop
-			current = num % 10; //to get the last digit from the integer
-			reverse = (reverse* 10) + current; //to reverse the number
-			num = num / 10; //to remove remainder
+	//method to display counts
+	public static void displayCounts(int[]counts) {
+		//condition to display result
+		for(int i = 0; i < 50; i++) {
+					
+			//displayed if the number at i element = 1(time)
+			if(counts[i] == 1)
+				System.out.println(i+1 + " occurs " + counts[i] + " time");
 			
+			//displayed if the number at i element > 1(times)
+			else if(counts[i] > 1)
+				System.out.println(i+1 + " occurs " + counts[i] + " times");	
 		}
-		return reverse;
 	}
 	
-	//method to return true if the number is a palindrome
-	public static boolean isPalindrome(int num) {
-		//to declare and initialized variables
-		int revNum = reverse(num);
-		boolean isPalindrome = false;
-		
-		//condition to return number as a palindrome
-		if(num == revNum)
-			isPalindrome = true;
-		
-		return isPalindrome;
-	
-	}
-	
-	//main method to test the program
 	public static void main(String[] args) {
-		//to create a scanner
 		Scanner input = new Scanner(System.in);
-		//to prompt users to enter a number
-		System.out.print("Enter a number : ");
-		int i = input.nextInt();
-		//let j be the reversal number of i(using reverse method)
-		int j = reverse(i);
-		//to check if i is a palindrome(using isPalindrome method)
-		if(isPalindrome(i))
-			System.out.print(i + " = " + j + " (a Palindrome)");
-		else
-			System.out.print(i + " != " + j + " (not a Palindrome)");
+		//to set array size
+		int[]counts = new int[50];
+		
+		//prompt users to enter integers between 1 and 50
+		System.out.print("Enter the integers between 1 and 50 : ");
+		
+		//this input will become the first number insert to the array
+		int num = input.nextInt();
+		
+		//loop will continue until users enter 0
+		while(num != 0) {
 			
+			//to check if the number entered is less than 50
+			if(num <= 50)
+				//assign the first input at element(number-1) and count as 1 occurrence
+				counts[num-1]++;
+			
+		//to continue the loop, the next number must be entered
+		num = input.nextInt();
+		}
+		//call method to display counts
+		displayCounts(counts);
 		input.close();
-
 	}
-
 }
